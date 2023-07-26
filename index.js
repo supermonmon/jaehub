47e0ce0543a24889dde3302e566ec1cb6cfb7372
Richmond <richmondlavadia@gmail.com>
2023-07-26 20:19:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#hero-text-emphasize', {
+    strings: ['Web Service Solution.'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true,
+    smartBackspace: true,
+    backDelay: 2500,
+  });
