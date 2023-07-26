e6a55e49442bb7a8e174df3d682d617755982790
Richmond <richmondlavadia@gmail.com>
2023-07-26 23:42:28 +0800

    Added responsive layouts

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const navEl = document.querySelector('.navbar');
+        window.addEventListener('scroll', () => {
+            if(window.scrollY >= 56){
+                navEl.classList.add('navbar-scrolled');
+            }
+            else
+                navEl.classList.remove('navbar-scrolled');        
+        });
+
 var typed = new Typed('#hero-text-emphasize', {
     strings: ['Web Service Solution.'],
     typeSpeed: 70,
