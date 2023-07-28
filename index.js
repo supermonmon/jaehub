64c0333a14189065ee9fe569186495989934020c
Richmond <richmondlavadia@gmail.com>
2023-07-29 00:48:31 +0800

    Finished Layout

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const navEl = document.querySelector('.navbar');
         window.addEventListener('scroll', () => {
-            if(window.scrollY >= 56){
+            if(window.scrollY >= 36){
                 navEl.classList.add('navbar-scrolled');
             }
             else
-                navEl.classList.remove('navbar-scrolled');        
+                navEl.classList.remove('navbar-scrolled');
         });
+
 
 var typed = new Typed('#hero-text-emphasize', {
     strings: ['Web Service Solution.'],
